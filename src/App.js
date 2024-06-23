b987b651c96ca5e62b16abc3d79f05f63514a757
Tafari Excell <excell.tafari@gmail.com>
2024-06-23 13:55:29 -0400

    imported Home and Navbar to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-// import logo from './logo.svg';
-import './App.css';
+import Navbar from './Components/Navbar'
+import Home from './Components/Home'
+// import './App.css';
 
 function App() {
   return (
     <div className="Frontpage">
-      <header>
-        GoodPies
-      </header>
+      <Navbar />
+      <Home />
     </div>
   );
 }
